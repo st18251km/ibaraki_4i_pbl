aa079f81c6c8a9a5b191ea1239e68656edd7f9fe
st18251km <st18251km@gm.ibaraki-ct.ac.jp>
2021-11-12 15:32:52 +0900

    ぷっしゅ

--- src/main.py
+++ src/main.py
@@ -216,6 +216,21 @@ def add_student():
             "status": "failed"
         })
 
+#週に1回プッシュ通知
+@app.route("/push_toban", methods=['POST'])
+def push_toban():
+    group_id = "1111"
+    try:
+        line_bot_api.push_message(group_id, TextSendMessage(text=f"(PUSH)今週の週番は{find_student()}です。水曜日は掃除をしてください。"))
+        return jsonify({
+            "status": "success"
+        })
+    except Exception as e:
+        print(e)
+        return jsonify({
+            "status": "failed"
+        })
+
 
 @app.route("/callback", methods=['POST'])
 def callback():
